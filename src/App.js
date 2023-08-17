f71f1ac03ddd8fdbe57bcc3ec4725a7eca4890fd
lupengwa <steve.wang.apple@gmail.com>
2023-08-16 21:50:05 -0700

    update my jarvis to be a little more useful

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import Checkout from "./components/checkout/Checkout";
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const cards = [
+    {
+      title: 'Card 1',
+      content: 'It only takes one second for you to lose the whole thing.'
+    },
+    {
+      title: 'Card 2',
+      content: 'When that second comes, Stay there physically, but get out mentally to rethink.'
+    },
+  ];
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        <h1>汪记</h1>
-        <Checkout />
+      <div className='card'>
+        <p>{cards[currentIndex].content}</p>
       </div>
     </div>
   );
