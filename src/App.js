2c8e1b69a58adfbf1ea7a18b98d1f344a7c4e3fa
lupengwa <steve.wang.apple@gmail.com>
2023-08-31 15:40:38 -0700

    Added api call template Updated status

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Stage1 from "./components/flow/Stage1";
 import Stage2 from "./components/flow/Stage2";
 import Stage3 from "./components/flow/Stage3";
 import End from "./components/flow/End";
-import RandNum from "./components/index/Ranindex";
 import ProgressBar from "./components/progress/ProgressBar";
+import api from './components/api/axiosConfig';
 
 const cards = [
     {
@@ -45,13 +45,24 @@ const generateShuffledNumbers = () => {
 // let numbers = generateShuffledNumbers();
 // console.log(numbers)
 const numbers = [35,90,48,38,114,122,70,102,23,36,42,139,141,87,1,20,115,62,128,94,117,133,17,52,91,46,89,71,26,41,55,108,32,83,39,30,127,84,77,81,21,76,25,24,110,3,93,116,64,123,58,107,85,120,2,129,37,16,136,27,104,99,8,13,29,112,11,6,66,101,9,67,95,96,135,33,63,45,78,31,28,79,18,111,44,144,56,61,118,125,49,12,100,54,5,4,98,130,74,82,138,72,88,97,15,43,113,86,50,121,57,7,131,132,14,69,65,124,19,80,137,22,105,59,73,92,53,47,106,10,143,134,51,126,75,34,142,40,140,109,103,68,60,119]
-
+const progresBars = [{"name":"Job Seeking", "val":100}, {"name":"Coding", "val":40}, {"name":"System Design", "val":0}, {"name":"Leadership", "val":20}]
 
 
 function App() {
 
     const [numIdx, setNumIndex] = useState(0);
     const [round, setRound] = useState(1);
+
+    const getInfo = async () => {
+        try {
+            const response = await api.get("/api/v1/movies");
+            console.log(response);
+            //set state to pass the data to app state
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
 
     const handleNextRandom = () => {
         if(numIdx+1 < numbers.length) {
@@ -68,6 +79,7 @@ function App() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
+        // call getInfo(); in a place that only load once
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
         }, 3000); // Change image every 3 seconds
@@ -96,7 +108,6 @@ function App() {
         currentStageComponent = <End onNext={handleNext} />;
     }
 
-
     return (
         <div className="App">
             <div className='slideshow'>
@@ -106,10 +117,11 @@ function App() {
                 {currentStageComponent}
             </div>
             <div>
-                <ProgressBar name="Job Seeking" progress={2} />
-                <ProgressBar name="Coding" progress={0} />
-                <ProgressBar name="System Design" progress={0} />
-                <ProgressBar name="Leadership" progress={0} />
+                {
+                    progresBars.map((progressBar) => {
+                        return <ProgressBar key= {progressBar.name} name={progressBar.name} progress={progressBar.val}/>
+                    })
+                }
             </div>
 
             {/*<div>*/}
